0d93c6a857769f4ed3b044df661582b51c0d1073
HAN <QQ1554646899@outlook.com>
2023-06-13 14:56:01 +0800

    1

--- src/main/java/filter/CharacterEncodingFilter.java
+++ src/main/java/filter/CharacterEncodingFilter.java
@@ -3,23 +3,24 @@ package filter;
 import javax.servlet.*;
 import java.io.IOException;
 
-public class CharacterEncodingFilter implements Filter{
-    public void init(FilterConfig filterConfig) throws ServletException {
+public class CharacterEncodingFilter implements Filter {
+        public void init(FilterConfig filterConfig) throws ServletException {
 
-    }
+        }
 
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        servletRequest.setCharacterEncoding("utf-8");
-        servletResponse.setCharacterEncoding("utf-8");
-        servletResponse.setContentType("text/html;charset=UTF-8");
+        @Override
+        public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+            servletRequest.setCharacterEncoding("utf-8");
+            servletResponse.setCharacterEncoding("utf-8");
+            servletResponse.setContentType("text/html;charset=UTF-8");
 
-        //让我们的请求继续走，如果不写，程序到这里就被拦截停止！
-        filterChain.doFilter(servletRequest,servletResponse);
-    }
+            //让我们的请求继续走，如果不写，程序到这里就被拦截停止！
+            filterChain.doFilter(servletRequest,servletResponse);
+        }
 
-    @Override
-    public void destroy() {
+        @Override
+        public void destroy() {
 
-    }
+        }
+
 }
